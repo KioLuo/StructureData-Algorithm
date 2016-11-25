cb0e6e56bd82f45efbcfaf1e219300ebe33518eb
Ganlee-QY <Ganlee-QY@DESKTOP-KVICUQK>
2016-11-25 17:02:51 +0800

    2016.11.25-17:02

--- ChuZhanXuLieTongJi/ChuZhanXuLieTongJi/ChuZhanXuLieTongJi.cpp
+++ ChuZhanXuLieTongJi/ChuZhanXuLieTongJi/ChuZhanXuLieTongJi.cpp
@@ -2,23 +2,52 @@
 //
 
 #include "stdafx.h"
-#include <iostream>
 #include "Stack.h"
+#include <iostream>
+#include <vector>
 using namespace std;
+
+vector<int> inArray, outArray, reposArray;
+
+bool PushStack(Stack<int> stack)
+{
+	if (inArray[0] == NULL)
+		return false;
+	else {
+		stack.Push(inArray[0]);
+		return true;
+	}
+}
+
+bool PopStack(Stack<int> stack, int& buffer)
+{
+	if (stack.IsEmpty())
+		return false;
+	else {
+		stack.Pop(buffer);
+		return true;
+	}
+}
+
+void StoreArray(vector<int> outArray)
+{
+	reposArray.push_back(0);
+	int temp = 0;
+	for (int i = 0; i < outArray.size(); i++) {
+		temp 
+	}
+}
+
+bool IfRepeat(vector<int> outArray)
+{
+	for (int i = 0; i < reposArray.size(); i++) {
+		if (reposArray[i] == outArray)
+	}
+}
 
 int main()
 {
-	int a;
-	Stack<int> stack(1);
-	stack.Push(2);
-	stack.Push(3);
-	stack.Top(a);
-	cout << a << endl;
-	a = stack.GetCount();
-	cout << a << endl;
-	stack.Pop(a);
-	int b = stack.GetCount();
-	cout << a << b << endl;
+
     return 0;
 }
 
