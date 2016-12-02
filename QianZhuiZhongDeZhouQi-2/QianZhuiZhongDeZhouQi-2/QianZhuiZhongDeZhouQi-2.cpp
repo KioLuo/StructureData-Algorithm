ce0b55d295740f2f48a42d884262bd5126c79af3
qy_lu <qy_lu@DESKTOP-KM9I8HH>
2016-12-02 21:36:45 +0800

    2016.12.2-21:36

--- QianZhuiZhongDeZhouQi-2/QianZhuiZhongDeZhouQi-2/QianZhuiZhongDeZhouQi-2.cpp
+++ QianZhuiZhongDeZhouQi-2/QianZhuiZhongDeZhouQi-2/QianZhuiZhongDeZhouQi-2.cpp
@@ -2,7 +2,29 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
 
+int* FindNext(string str)
+{
+	int i, k;
+	int strLen = str.length();
+	int* next = new int[strLen];
+	next[0] = -1;
+	k = -1;
+	i = 0;
+	while (i < strLen)
+	{
+		while ((k >= 0) && (str[i] != str[k]))
+		{
+			k = next[k];
+		}
+		k++;
+		i++;
+		next[i] = k;
+	}
+}
 
 int main()
 {
